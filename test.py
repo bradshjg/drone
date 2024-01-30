bbaa6305a587071b1c77afb75bc03bfdd53b4985
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2024-01-29 22:14:57 -0600

    this works...now to decode!
    * I think it actually stops sending data pretty
      quickly, so probably need to switch message
      types and make this async.

--- test.py
+++ test.py
@@ -1,19 +1,21 @@
 import socket
 
 # create UDP socket
-s = socket.socket(socket.AF_INET, socket.DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+local_ipaddr = '192.168.169.2'
+local_port = 56789
 ipaddr = '192.168.169.1'
 port = 8800
-# send data to server
-init_message = bytes.fromhex('ef08080001000000')
-s.bind((ipaddr, port))
+
+init_message = bytes.fromhex('ef000400')
+s.bind((local_ipaddr, local_port))
 print("send init message")
-for i in range(1, 4):
-  s.send(init_message, (ipaddr, port))
+for i in range(1, 10):
+  s.sendto(init_message, (ipaddr, port))
 
 print("receiving data")
-fp = open('test.stream', 'w')
+fp = open('test.stream', 'wb')
 while True:
   data = s.recv(1024)
   fp.write(data)
